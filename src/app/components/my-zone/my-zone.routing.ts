79dab275e45728c6e2f8329fce9f515fa9560fb6
Diego Bello <diegosferas@gmail.com>
2020-02-17 22:02:54 -0500

    Se creo el primer paso para filtrar los resultados no puedo seguir por que me falta el backend, en la imagen hace falta mejorar el css para no repetir codigo talvez manejarlo a solo clases

--- src/app/components/my-zone/my-zone.routing.ts
+++ src/app/components/my-zone/my-zone.routing.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
     component: MyZoneComponent,
     children: [
       { path: 'docs/new', loadChildren: () => import('./../docs/new-docs/new-docs.module').then(m => m.NewDocsModule) },
+      { path: 'search', loadChildren: () => import('./../search/search.module').then(m => m.SearchModule) },
     ]
   }
 ];
