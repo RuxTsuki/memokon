bbfc6f70071e6c08b49fed3af2a7bee0380e595f
Diego Bello <diegosferas@gmail.com>
2020-02-25 22:02:14 -0500

    Changes in the visualization, i need to change the animation when the content init, also i need to separate in several components so i needed to see the accordion to know how can i do it.

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-notes',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesComponent implements OnInit {
 
+  show = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  noteToggle() {
+    this.show = !this.show;
+
   }
 
 }
--- src/app/components/notes/notes.module.ts
+++ src/app/components/notes/notes.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotesComponent } from './notes.component';
 import { NotesRoutingModule } from './notes.routing';
-
+import { MatRippleModule } from '@angular/material/core';
 
 
 @NgModule({
   declarations: [NotesComponent],
   imports: [
     CommonModule,
-    NotesRoutingModule
+    NotesRoutingModule,
+    MatRippleModule
   ],
   exports: [
     NotesComponent
